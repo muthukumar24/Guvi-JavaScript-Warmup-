13e4a1a6658dd7675e7eef924dc9f77e5d4fb658
MuthukumarS07 <smuthukumar2408@gmail.com>
2024-02-25 17:21:39 +0530

    first commit

---
+++ Java Script code/GUVI WarmUp - 5.js
@@ -0,0 +1,14 @@
+/* Create a function that takes an array and returns the first element. */
+
+function getFirstElement(arr) {
+	///Your code Starts here 
+	let arrElementOne = arr[0]
+	return arrElementOne
+	///Your code Ends here
+}
+/*
+Lines For TestCase
+*/
+console.log(getFirstElement([1, 2, 3]));
+console.log(getFirstElement([80, 5, 100]));
+console.log(getFirstElement([-500, 0, 50]));
